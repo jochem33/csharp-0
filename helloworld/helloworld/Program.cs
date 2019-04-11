ba8380953bdf68f4b232de26426b61a8e0838cd6
Jochem Baas <jochemjuriaan.baas@coderclass.nl>
2019-04-11 09:33:52 +0200

    Einopdracht lesbrief 3

---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace helloworld
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            float answer = 0.0f;
+            int operatorIndex = -1;
+            string operatorStr = "*";
+
+
+            int firstPartInt = 0;
+            int secondPartInt = 0;
+
+
+            Console.WriteLine("Geef een som: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input);
+
+            if (input.Contains("*"))
+            {
+                operatorIndex = input.IndexOf("*");
+                operatorStr = "*";
+            }
+            else if (input.Contains("/"))
+            {
+                operatorIndex = input.IndexOf("/");
+                operatorStr = "/";
+            }
+            else if (input.Contains("+"))
+            {
+                operatorIndex = input.IndexOf("+");
+                operatorStr = "+";
+            }
+            else if (input.Contains("-"))
+            {
+                operatorIndex = input.IndexOf("-");
+                operatorStr = "-";
+            }
+
+
+            if (operatorIndex >= 1)
+            {
+                string firstPart = input.Substring(0, operatorIndex);
+                string secondPart = input.Substring(operatorIndex + 1);
+
+                firstPartInt = Convert.ToInt32(firstPart);
+                secondPartInt = Convert.ToInt32(secondPart);
+
+            }
+
+            switch (operatorStr)
+            {
+                case "*":
+                    answer = firstPartInt * secondPartInt;
+                    break;
+                case "/":
+                    if (secondPartInt == 0)
+                    {
+                        Console.WriteLine("Hee, dat is illegaal!");
+                        break;
+                    }
+                    answer = firstPartInt / secondPartInt;
+                    break;
+                case "+":
+                    answer = firstPartInt + secondPartInt;
+                    break;
+                case "-":
+                    answer = firstPartInt - secondPartInt;
+                    break;
+            }
+
+            Console.WriteLine(answer);
+        }
+    }   
+}
