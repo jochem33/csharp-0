a2997fe4c2f20491b624b62bdfc9ff5e7eb93d9c
Jochem Baas <jochemjuriaan.baas@coderclass.nl>
2019-04-18 08:53:00 +0200

    [add] fibonaci program

---
+++ csharp/fibonaci/fibonaci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace fibonaci
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int firstNumber = 1;
+            int secondNumber = 2;
+
+            int[] numbers = new int[20];
+
+            for (int i = 1; i < 20; i++)
+            {
+                int thirdNumber = firstNumber + secondNumber;
+
+                firstNumber = secondNumber;
+                secondNumber = thirdNumber;
+
+                Console.WriteLine(secondNumber);
+                numbers[i] = secondNumber;
+            }
+        }
+    }
+}
