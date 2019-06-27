385e978f56604c910bd437c0a449292c2d01db6a
Jochem Baas <jochemjuriaan.baas@coderclass.nl>
2019-06-27 09:14:08 +0200

    [add] telefoons programma is af

---
+++ csharp/telefoons/telefoons/MainClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace telefoons
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Mobiel iphone = new Mobiel("iphone", "XS", "IOS", "Steve Jobs", 999.99f);
+            Mobiel samsung = new Mobiel("galaxy", "S7", "Android", "Kom Dong Jin", 798.99f);
+            Mobiel google = new Mobiel("pixel", "3", "Android", "Sundar Pichai", 865.59f);
+
+            Mobiel telefoon = iphone;
+
+            Console.WriteLine(iphone.soort + " door " + iphone.CEO);
+            Console.WriteLine(samsung.soort + " door " + samsung.CEO);
+            Console.WriteLine(google.soort + " door " + google.CEO);
+
+
+            Console.WriteLine("Welke mobiel wil je kopen? ");
+            string welkeMobiel = Console.ReadLine();
+            Console.WriteLine("Hoeveel wil je kopen? ");
+            int hoeveel = Convert.ToInt32(Console.ReadLine());
+
+            if (welkeMobiel == iphone.soort || welkeMobiel == iphone.model)
+            {
+                telefoon = iphone;
+            }
+            else if (welkeMobiel == samsung.soort || welkeMobiel == samsung.model)
+            {
+                telefoon = samsung;
+            }
+            else if (welkeMobiel == google.soort || welkeMobiel == google.model)
+            {
+                telefoon = google;
+            }
+
+            float price = telefoon.price * hoeveel;
+            Console.WriteLine("Dat kost: " + price);
+            if(price > 900)
+            {
+                Console.WriteLine("Gefeliciteerd, het kost meer dan 900 euro dus je krijgt gratis verzending!");
+            }
+        }
+    }
+}
---
+++ csharp/telefoons/telefoons/Mobiel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace telefoons
+{
+    public class Mobiel
+    {
+        public string soort;
+        public string model;
+        public string os;
+        public string CEO;
+        public float price;
+
+        public Mobiel(string nSoort, string nModel, string nOs, string nCEO, float nPrice)
+        {
+            soort = nSoort;
+            model = nModel;
+            os = nOs;
+            CEO = nCEO;
+            price = nPrice;
+        }
+    }
+}
